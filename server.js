77d85414c008ec4b2d5298171160f834822c316d
Vũ Mạnh Hùng <80694706+manhhung311@users.noreply.github.com>
2022-10-01 16:25:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ app.set('view engine','ejs');
 app.get('/', (req, res)=> {
     res.render('index')
 })
-app.get('/themsinhvienmoi' , (req, res)=> {
-     res.render('index')
-});
-
-app.get
 
 var port = process.env.PORT || 3000
 app.listen(port, (err)=> {
